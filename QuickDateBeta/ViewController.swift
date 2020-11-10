4768a5f154860c1d39604eb8960cd487534f5510
vitoyaokatsu <vitoyaokatsu@gmail.com>
2020-11-10 23:16:10 +0900

    Initial Commit

---
+++ QuickDateBeta/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuickDateBeta
+//
+//  Created by Katsuhiko Yao on 2020/11/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
